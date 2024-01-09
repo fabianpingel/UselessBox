5bc5c87ebccf2a29f966bd14f0c9e9319126de42
Fabian Pingel <96516664+fabianpingel@users.noreply.github.com>
2024-01-09 16:04:15 +0100

    Add files via upload

---
+++ sketch_jan9a.ino
@@ -0,0 +1,32 @@
+/********************************
+*   NutzloseBox für Bernhard    *
+*            AI-Team
+********************************/
+
+// Includes
+#include <Servo.h>
+
+// Setup
+Servo myservo_deckel; // Servo für Deckel initialisieren
+Servo myservo_finger; // Servo für Finger initialisieren
+
+const int swPin = 2;
+
+int pos = 0;
+int action = 0;
+
+
+// Initialisierung einiger Funktionen
+void setup() {
+  // put your setup code here, to run once:
+
+
+
+}
+
+
+// MAIN
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
